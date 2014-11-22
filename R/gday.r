a8304f1d939058df2ccc0f273a1ba20e2f56a0dd
Beryl <beryl.c.zhuang@gmail.com>
2014-11-22 15:10:55 -0800

    added new tests

--- R/gday.r
+++ R/gday.r
@@ -10,16 +10,28 @@
 #' Suffer no more! You can now ask R directly, without tempting yourself
 #' by firing up your web browser.
 #'
-#' @param team.name The name of your team
-#' @return Logical \code{TRUE} if \code{team.name} has a NHL game today,
+#' @param team The name of your team
+#' @param date The date you want to check (in YYYY-MM-DD format),
+#' default vaule is system date
+#' @return Logical \code{TRUE} if \code{team} has a NHL game today,
 #' \code{FALSE} otherwise
-#' @note case in \code{team.name} is ignored
+#' @note case in \code{team} is ignored
 #' @export
 #' @examples
 #' gday("canucks")
-#' gday("Bruins")
-gday <- function(team.name){
-  url  <- paste0('http://live.nhle.com/GameData/GCScoreboard/',
-                 Sys.Date(), '.jsonp')
-  grepl(team.name, RCurl::getURL(url), ignore.case=TRUE)
+#' gday("Blues", "2014-11-22")
+#'
+
+# gday <- function(team){
+#   url  <- paste0('http://live.nhle.com/GameData/GCScoreboard/',
+#                  Sys.Date(), '.jsonp')
+#   grepl(team, RCurl::getURL(url), ignore.case = TRUE)
+# }
+
+# add date argument
+gday <- function(team = "canucks", date = Sys.Date()){
+#	assert_that()
+	url  <- paste0('http://live.nhle.com/GameData/GCScoreboard/',
+								 date, '.jsonp')
+	grepl(team, RCurl::getURL(url), ignore.case = TRUE)
 }
--- tests/testthat/test_me.R
+++ tests/testthat/test_me.R
@@ -1,7 +1,16 @@
+# tests for gday()
+
+#test for ignored case for team names
 test_that("case is ignored", {
   expect_equal(gday("canucks"), gday("CANUCKS"))
 })
 
+# test if the input team is not a team name
 test_that("Seattle does not have a NHL team", {
   expect_false(gday("seattle"))
+})
+
+test_that("Blues has a game against Senators on 2014-11-22", {
+	expect_true(gday(team = "blues",   date = "2014-11-22"))
+	expect_true(gday(team = "senators", date = "2014-11-22"))
 })
